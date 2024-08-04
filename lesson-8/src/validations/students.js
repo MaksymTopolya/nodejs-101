dbc960ba817293bc52686c244cc0a8b49498f988
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-08-04 12:37:48 +0300

    Update students.js

--- lesson-8/src/validations/students.js
+++ lesson-8/src/validations/students.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const studentSchema = Joi.object({
   name: Joi.string().min(3).max(30).required().messages({
     'string.base': 'Name should be a string',
-    'string.min': 'Name should be at least {#limit}',
+    ' string.min': 'Name should be at least {#limit}',
     'string.max': 'Name should be at most {#limit}',
   }),
   gender: Joi.string().valid('male', 'female').required().messages({
